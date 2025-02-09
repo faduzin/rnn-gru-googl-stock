b46f8d2f61772622bf70ede3b65bf04ae8307b95
faduzin <eric.fadul@gmail.com>
2025-02-09 09:46:31 -0300

    preprocessing script

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -82,9 +82,9 @@ def preprocess_data(df,
     return df
 
 
-def split_data(X, y, test_size=0.2):
+def split_data(X, y, test_size=0.2, shuffle=True):
     try:
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42, shuffle=shuffle)
         print('Data split successfully.')
         return X_train, X_test, y_train, y_test
     except:
